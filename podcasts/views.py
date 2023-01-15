672d1db953b369d6bc8b96107a1a7b761ec63ad4
Christian Loving <christian70401@gmail.com>
2023-01-14 20:45:46 -0600

    django tutorial

--- podcasts/views.py
+++ podcasts/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+
+from .models import Episode
 
 # Create your views here.
+class HomePageView(ListView):
+    template_name = "homepage.html"
+    molel = Episode
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["episodes"] = Episode.objects.filter().order_by("-pub_date")[:10]
+        return context
+
+    def get_queryset(self):
+        return Episode.objects.all()
